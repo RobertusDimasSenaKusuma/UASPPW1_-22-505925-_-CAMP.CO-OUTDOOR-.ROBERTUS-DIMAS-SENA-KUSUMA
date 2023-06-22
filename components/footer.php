5b70ae13b9a788b4c1b3a64d2a454c0859e4b315
robertus2003 <111260478+robertus2003@users.noreply.github.com>
2023-06-22 01:08:47 -0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> Tentang Kami</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Toko</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Kontak Kami</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> keranjang</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orderan</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:085875997242"><i class="fas fa-phone"></i> +62 85875997242</a>
+         <a href="tel:081224398066"><i class="fas fa-phone"></i>+62 91224398066</a>
+         <a href="mailto:r.dimassenakusuma@gmail.com"><i class="fas fa-envelope"></i> r.dimassenakusuma@gmail.com</a>
+         <a href="https://goo.gl/maps/5iQiUGBEnRxq2V8d8"><i class="fas fa-map-marker-alt"></i> Yogyakarta, Indonesia - 56298 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>robertus dimas sena kusuma</span> | all rights reserved!</div>
+
+</footer>
